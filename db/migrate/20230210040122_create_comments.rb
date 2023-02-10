77bee88c4345b6e1f7dec2a72e4a77b1697adec1
giangleee <giangle602@gmail.com>
2023-02-10 11:03:42 +0700

    Add comments

--- db/migrate/20230210040122_create_comments.rb
+++ db/migrate/20230210040122_create_comments.rb
@@ -4,6 +4,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
       t.integer :user_id
       t.integer :video_id
       t.string :comment
+      t.datetime :time
 
       t.timestamps
     end
