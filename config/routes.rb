3a481a3abd013d8c98e469415080750e9f50f16b
giangleee <giangle602@gmail.com>
2023-02-10 10:34:34 +0700

    Build base project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :book_reviews
+  get 'static_pages/home'
+  devise_for :users
+  devise_for :models
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
