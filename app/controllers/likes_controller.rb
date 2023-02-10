1d9cffee1f999a2bbcb8c72006795d443f518e4b
ToanDL1 <toanlevan0123@gmail.com>
2023-02-10 23:08:27 +0700

    add Like Controler View Model Test

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,70 +1,38 @@
 class LikesController < ApplicationController
-  before_action :set_like, only: %i[ show edit update destroy ]
 
-  # GET /likes or /likes.json
-  def index
-    @likes = Like.all
-  end
+  before_action :find_video
+  before_action :find_like, only: [:destroy]
 
-  # GET /likes/1 or /likes/1.json
-  def show
-  end
-
-  # GET /likes/new
-  def new
-    @like = Like.new
-  end
-
-  # GET /likes/1/edit
-  def edit
-  end
-
-  # POST /likes or /likes.json
   def create
-    @like = Like.new(like_params)
-
-    respond_to do |format|
-      if @like.save
-        format.html { redirect_to like_url(@like), notice: "Like was successfully created." }
-        format.json { render :show, status: :created, location: @like }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @like.errors, status: :unprocessable_entity }
-      end
-    end
+      if already_liked?
+          flash[:notice] = "You can't like more than once"
+        else
+          @video.likes.create(user_id: current_user.id)
+        end
+        redirect_to video_path(@video)
   end
 
-  # PATCH/PUT /likes/1 or /likes/1.json
-  def update
-    respond_to do |format|
-      if @like.update(like_params)
-        format.html { redirect_to like_url(@like), notice: "Like was successfully updated." }
-        format.json { render :show, status: :ok, location: @like }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @like.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /likes/1 or /likes/1.json
   def destroy
-    @like.destroy
-
-    respond_to do |format|
-      format.html { redirect_to likes_url, notice: "Like was successfully destroyed." }
-      format.json { head :no_content }
-    end
+      if !(already_liked?)
+        flash[:notice] = "You didn't even press like the video!"
+      else
+        @like.destroy
+      end
+      redirect_to video_path(@video)
   end
 
+  def find_like
+      @like = @video.likes.find(params[:id])
+  end
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_like
-      @like = Like.find(params[:id])
-    end
+     
+  def find_video
+       @video = Video.find(params[:video_id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def like_params
-      params.fetch(:like, {})
-    end
+
+  def already_liked?
+      Like.where(user_id: current_user.id, video_id: params[:video_id]).exists?
+  end
+
 end
