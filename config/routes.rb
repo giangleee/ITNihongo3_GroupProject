dd41fa6da77b5bda0c3c7274098474e9eb7a36d9
giangleee <giangleee>
2023-02-12 21:42:01 +0700

    Update code

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :impressions
   get 'static_pages/home'
   resources :categories
   resources :comments
--- db/migrate/20230210035515_add_publish_likes_and_views_to_videos.rb
+++
@@ -1,6 +0,0 @@
-class AddPublishLikesAndViewsToVideos < ActiveRecord::Migration[6.0]
-   def change
-     add_column :videos, :likes, :integer, default: 0
-     # add_column :videos, :views, :integer, default: 0
-   end
- end
